8717cae6faaf97a5a7c24ed602b9bbb327042dd2
Sohaib Shah <turbosohaib1234@gmail.com>
2025-03-11 17:29:42 +0500

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    port: 8001,
+    port: 8001, // Set your port
+  },
+  preview: {
+    port: 8001, // Ensure preview also runs on the correct port
+    allowedHosts: ["simple3dwebsite.umairshah.dev"],
   },
 });
